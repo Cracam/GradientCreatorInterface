c45b5d384893a4cd7f4361f9f82b5f07241f1114
camil <camil@PC_Camille>
2024-12-03 17:55:50 +0100

    change the entry for image to opacity array and add a hashmap to save it

--- src/GradientCreatorInterface/GradientCreatorLeftRight.java
+++ src/GradientCreatorInterface/GradientCreatorLeftRight.java
@@ -1,47 +1,29 @@
 package GradientCreatorInterface;
 
-
 import java.awt.Color;
-import java.awt.GradientPaint;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.image.Image;
 
 public class GradientCreatorLeftRight extends GradientCreator {
 
-    public GradientCreatorLeftRight() {
-        super("Gauche-Droite", true) ;
-    }
+        public GradientCreatorLeftRight() {
+                super("Gauche-Droite", true);
+        }
 
+        @Override
+        public float[][] generateBlendTable(int x_dim, int y_dim, Color color1, Color color2, double colorIntensityParam, double param1, double param2) {
 
+                colorIntensityParam = convertBlendCoeff(colorIntensityParam);
 
-    @Override 
-   public BufferedImage generateColoredImage(BufferedImage image_in, Color color1, Color color2, double colorIntensityParam, double param1, double param2) {
+                float[][] blendTable = new float[x_dim][y_dim];
 
-             colorIntensityParam=convertBlendCoeff(colorIntensityParam);
-        
-         
-         int x_dim=image_in.getWidth();
-         int y_dim=image_in.getHeight();
-         
-         float [][] blendTable= new float[x_dim][y_dim];
+                // Loop through each pixel of the input image
+                for (int x = 0; x < x_dim; x++) {
+                        float alphaBlend = (float) Math.pow((float) x / (x_dim - 1), colorIntensityParam);
+                        for (int y = 0; y < y_dim; y++) {
+                                blendTable[x][y] = alphaBlend;
+                        }
+                }
+                return blendTable;
 
-     
-             // Loop through each pixel of the input image
-             for (int x = 0; x < x_dim; x++) {
-                      float alphaBlend = (float) Math.pow((float) x / (x_dim - 1), colorIntensityParam);
-                      for (int y = 0; y < y_dim; y++) {
-                               blendTable[x][y] = alphaBlend;
-                      }
-             }
-             
-
-
-
-             
-         return generateImage(blendTable,color2,color1);
-      
-    }
+        }
 }
 //blend x2 if >1 *15 else nothing
--- src/GradientCreatorInterface/GradientCreatorUpDown.java
+++ src/GradientCreatorInterface/GradientCreatorUpDown.java
@@ -1,43 +1,29 @@
 package GradientCreatorInterface;
 
-
 import java.awt.Color;
-import java.awt.image.BufferedImage;
 
 public class GradientCreatorUpDown extends GradientCreator {
 
-    public GradientCreatorUpDown() {
-        super("Haut-bas", true) ;
-    }
+        public GradientCreatorUpDown() {
+                super("Haut-bas", true);
+        }
 
+        @Override
+        public float[][] generateBlendTable(int x_dim, int y_dim, Color color1, Color color2, double colorIntensityParam, double param1, double param2) {
 
+                colorIntensityParam = convertBlendCoeff(colorIntensityParam);
 
-    @Override 
-   public BufferedImage generateColoredImage(BufferedImage image_in, Color color1, Color color2, double colorIntensityParam, double param1, double param2) {
+                float[][] blendTable = new float[x_dim][y_dim];
 
-             colorIntensityParam=convertBlendCoeff(colorIntensityParam);
-        
-         
-         int x_dim=image_in.getWidth();
-         int y_dim=image_in.getHeight();
-         
-         float [][] blendTable= new float[x_dim][y_dim];
+                // Loop through each pixel of the input image
+                for (int y = 0; y < y_dim; y++) {
+                        float alphaBlend = (float) Math.pow((float) y / (y_dim - 1), colorIntensityParam);
+                        for (int x = 0; x < x_dim; x++) {
+                                blendTable[x][y] = alphaBlend;
+                        }
+                }
+                return blendTable;
 
-     
-             // Loop through each pixel of the input image
-             for (int y = 0; y < y_dim; y++) {
-                      float alphaBlend = (float) Math.pow((float) y / (y_dim - 1), colorIntensityParam);
-                      for (int x = 0; x < x_dim; x++) {
-                               blendTable[x][y] = alphaBlend;
-                      }
-             }
-             
-
-
-
-             
-         return generateImage(blendTable,color2,color1);
-      
-    }
+        }
 }
 //blend x2 if >1 *15 else nothing
